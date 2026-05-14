94dda8f928b01fad69e76e1624e145280e36673e
GavinDiaper <gavindiaper@gmail.com>
2026-05-15 00:02:13 +0400

    Refactor survivor stress and hunger checks to utilize relaxation indicators and streamline HUD display

--- SAD_MOD_Lina_01/Code/ModLina_Config.lua
+++ SAD_MOD_Lina_01/Code/ModLina_Config.lua
@@ -17,7 +17,7 @@ local CONFIG_DEFAULTS = {
 
 	-- Thresholds for Advisor alerts
 	thresholds = {
-		stress = 60,      -- Alert if composite survivor stress score > this
+		stress = 60,      -- Alert if survivor stress > this (mood < 40)
 		hunger = 20,      -- Alert if survivor food < this
 		cloth = 20,       -- Alert if Cloth resource < this
 	},
